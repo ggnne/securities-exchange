752b1cea7aa6518e2f3074474a0d0df82aae4aa7
gguerri <giulio.guerri93@gmail.com>
2024-02-12 23:27:03 +0100

    Revert "temporary bug fix"
    This reverts commit 036470cd43157f0c858bac8d8a7ca5d861af5d57.

--- securities_exchange/__init__.py
+++ securities_exchange/__init__.py
@@ -4,9 +4,6 @@ __author__ = """Giulio Guerri"""
 __email__ = "giulio.guerri93@gmail.com"
 __version__ = "0.0.9"
 
-import logging
-
-logger = logging.getLogger(__name__) # to be removed
 
 from .securities_exchange import SecuritiesExchange
 from .enums import OrderType, OrderStatus, MarketSide
